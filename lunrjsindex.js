9d7f8049bdc0e6ae4aeea0d38be7ceed39b3d981
raifdmueller <raifdmueller@users.noreply.github.com>
2025-06-13 11:16:23 +0000

    deploy: 5970cdefa73fd32ce58e638594aa7438e7a5de4b

--- lunrjsindex.js
+++ lunrjsindex.js
@@ -2,6 +2,14 @@ var documents = [
 
 {
     "id": 0,
+    "uri": "arc42/arc42.html",
+    "menu": "arc42",
+    "title": "Architecture Documentation: Wardley Map Editor",
+    "text": " Table of Contents Architecture Documentation: Wardley Map Editor Architecture Documentation: Wardley Map Editor Ralf D. Müller &lt; ralf.d.mueller@gmail.com &gt; :doctype: book :toc: left :toclevels: 3 :sectnum: :sectanchors: :sectnums: :source-highlighter: highlight.js :icons: font :imagesdir: images Unresolved directive in &lt;stdin&gt; - include::01_introduction_and_goals.adoc[] Unresolved directive in &lt;stdin&gt; - include::02_architecture_constraints.adoc[] Unresolved directive in &lt;stdin&gt; - include::03_system_scope_and_context.adoc[] Unresolved directive in &lt;stdin&gt; - include::04_solution_strategy.adoc[] Unresolved directive in &lt;stdin&gt; - include::05_building_block_view.adoc[] Unresolved directive in &lt;stdin&gt; - include::06_runtime_view.adoc[] Unresolved directive in &lt;stdin&gt; - include::07_deployment_view.adoc[] Unresolved directive in &lt;stdin&gt; - include::08_concepts.adoc[] Unresolved directive in &lt;stdin&gt; - include::09_architecture_decisions.adoc[] Unresolved directive in &lt;stdin&gt; - include::10_quality_requirements.adoc[] Unresolved directive in &lt;stdin&gt; - include::11_risks_and_technical_debt.adoc[] Unresolved directive in &lt;stdin&gt; - include::12_glossary.adoc[] "
+},
+
+{
+    "id": 1,
     "uri": "search.html",
     "menu": "-",
     "title": "search",
@@ -9,7 +17,7 @@ var documents = [
 },
 
 {
-    "id": 1,
+    "id": 2,
     "uri": "lunrjsindex.html",
     "menu": "-",
     "title": "null",
